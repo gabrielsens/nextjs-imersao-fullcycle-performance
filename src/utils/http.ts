c0fe79b84092b1bc532313c73a246b3265fa45e7
Gabriel Sens <gabriel_sens@hotmail.com>
2022-08-24 09:56:28 -0300

    api proxy

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export const http = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+
+export const externalHttp = axios.create({
+  baseURL: process.env.EXTERNAL_API_URL,
+})
